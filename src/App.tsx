aac08cca669f551972c1e7421fb25bee13a0778e
darian baker bray <darianbakerbray@gmail.com>
2025-02-08 21:46:58 +0100

    added responsiveLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 function App() {
   return (
-    <div>
-      <h1>Game Hub</h1>
-    </div>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      md: `"nav nav" "aside main"`,
+    }}>
+      <GridItem gridArea="nav" bg="tomato">
+        Navigation
+      </GridItem>
+      <Show above="md">
+        <GridItem gridArea="aside" bg="papayawhip">
+          Sidebar
+        </GridItem>
+      </Show>
+      <GridItem gridArea="main" bg="lightblue">
+        Main Content
+      </GridItem>
+    </Grid>
   )
 }
 
