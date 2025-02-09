307afecac2735635e99d62baa191ba353f137ae4
darian baker bray <darianbakerbray@gmail.com>
2025-02-09 20:56:09 +0100

    added switch for dark and light theme

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react"
 import logo from "../assets/logo.webp"
+import ColorModeSwitch from "./ColorModeSwitch"
  
 const navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={10} bg="gray.800" color="white">
         <Image src={logo} boxSize="60px"/>
         <Text fontSize="2xl">Game Hub</Text>
+        <ColorModeSwitch />
     </HStack>
   )
 }
