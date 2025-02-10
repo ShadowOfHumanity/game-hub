75ca9d4655af7d34b716df61281662aa760967e0
darian baker bray <darianbakerbray@gmail.com>
2025-02-10 21:40:53 +0100

    cleaned up gamecards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -15,11 +15,12 @@ const GameCard = ({ game } : Props) => {
     <Card> 
         <Image src={imageFix(game.background_image, 600, 400)} alt={game.name} />
         <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-            <HStack justifyContent={'space-between'}>
+           
+            <HStack marginBottom={3} justifyContent={'space-between'}>
               <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
               <CriticScore score={game.metacritic} />
             </HStack>
+            <Heading fontSize='2xl'>{game.name}</Heading>
 
         </CardBody>
     </Card>
