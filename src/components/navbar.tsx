94cb394ea397d0a1d4a06d72158c1cb591272225
darian baker bray <darianbakerbray@gmail.com>
2025-02-10 20:36:25 +0100

    built search input

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
+import { HStack, Image } from "@chakra-ui/react"
 import logo from "../assets/logo.webp"
 import ColorModeSwitch from "./ColorModeSwitch"
+import SearchInput from "./SearchInput"
  
 const navbar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding={10} bg="gray.800" color="white">
+    <HStack padding={10} bg="gray.800" color="white">
         <Image src={logo} boxSize="60px"/>
-        <Text fontSize="2xl">Game Hub</Text>
+        <SearchInput />
         <ColorModeSwitch />
     </HStack>
   )
