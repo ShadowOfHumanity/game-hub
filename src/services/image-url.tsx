ee7c488d54d149e3ecbdaec3c623d1dda9e53071
darian baker bray <darianbakerbray@gmail.com>
2025-02-10 20:29:29 +0100

    Handle games w/no image

--- src/services/image-url.tsx
+++ src/services/image-url.tsx
@@ -1,8 +1,12 @@
+import placeholder from '../assets/placeholder.webp'
+
 const getCroppedImageUrl = (image: string , width: number, height: number) => {
-    if (!image) return '';
+    if(!image) return placeholder
+
     const target = 'media/'
-    const index = image.indexOf(target) +target.length; // some games dont have images
-    return image.slice(0, index) + `crop/${width}/${height}/` + image.slice(index);
+    const index =  image.indexOf(target) +target.length  // some games dont have images
+    
+    return image.slice(0, index) + `crop/${width}/${height}/` + image.slice(index) ;
 }
 
 export default getCroppedImageUrl;
